2d6851d9cd4b1aa466ca9b243783ddadf06658d2
manuelpalomeque <pjonathanmanuel@hotmail.com>
2022-01-01 11:31:47 -0300

    avance con pass_enc

--- Keylogger/pass_enc.py
+++ Keylogger/pass_enc.py
@@ -5,3 +5,13 @@ def generar_clave():
     with open('clave.encriptacion', 'wb') as file:
         file.write(key)
 
+def cargar_clave():
+    return open('clave.encriptacion', 'rb').read()
+
+generar_clave()
+
+key = cargar_clave()
+
+password = b'EjemploDeClave'
+
+file = open('clave.encriptada', 'wb')
